e6a3f6a9cc334e175edde851d81f1a28596ffd81
rodo111 <inforodolfo@yahoo.es>
2022-04-13 21:34:11 +0200

    PEC1_f1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
-// console.log('Hello world!');
-//const name = 'world';
-//console.log(`Hello ${name}`);
+//app.js solo se carga en la página "categoria"
+
+window.onload = function () {
+    var infancia = document.getElementById("infancia");
+    infancia.onmouseover = function () { document.getElementById("infancia").innerHTML = "Infancia"; };
+    infancia.onmouseout = function () {
+        document.getElementById("infancia").innerHTML = "Ver";
+    };
+
+    var filmografia = document.getElementById("filmografia");
+    filmografia.onmouseover = function () { document.getElementById("filmografia").innerHTML = "Filmografía"; };
+    filmografia.onmouseout = function () {
+        document.getElementById("filmografia").innerHTML = "Ver";
+    };
+
+    var politico = document.getElementById("politico");
+    politico.onmouseover = function () { document.getElementById("politico").innerHTML = "Político"; };
+    politico.onmouseout = function () {
+        document.getElementById("politico").innerHTML = "Ver";
+    };
+
+    var compositor = document.getElementById("compositor");
+    compositor.onmouseover = function () { document.getElementById("compositor").innerHTML = "Compositor"; };
+    compositor.onmouseout = function () {
+        document.getElementById("compositor").innerHTML = "Ver";
+    };
+
+    var exilio = document.getElementById("exilio");
+    exilio.onmouseover = function () { document.getElementById("exilio").innerHTML = "Exilio"; };
+    exilio.onmouseout = function () {
+        document.getElementById("exilio").innerHTML = "Ver";
+    };
+
+    var familia = document.getElementById("familia");
+    familia.onmouseover = function () { document.getElementById("familia").innerHTML = "Familia"; };
+    familia.onmouseout = function () {
+        document.getElementById("familia").innerHTML = "Ver";
+    };
+
+
+}
+
